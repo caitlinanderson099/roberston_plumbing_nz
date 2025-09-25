3219af4c74b4b60a842f962a2798b751e6f7336d
caitlinanderson099 <caitlin.anderson099@gmail.com>
2025-09-25 21:59:59 +1200

    started other pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,68 @@
 import React from 'react'
 import PageHeader from '../components/PageHeader'
+import { FaWrench } from 'react-icons/fa'
+import { FaTemperatureThreeQuarters } from 'react-icons/fa6'
+import { PiGridNineLight } from "react-icons/pi";
 
 
 const About = () => {
+
+  // Services Section
+  const ServiceSection = () => {
+    return (
+      <div className="services-section">
+        <div className="section-header">
+          <h2>Our services</h2>
+          <hr />
+        </div>
+        <div className="services-cont">
+          <div className="service-item">
+            <FaWrench />
+            <h4>Plumbing & Maintenance</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit...</p>
+          </div>
+
+          <div className="service-item">
+            <FaTemperatureThreeQuarters />
+            <h4>Central heating</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit...</p>
+          </div>
+
+          <div className="service-item">
+            <PiGridNineLight />
+            <h4>Drainlaying</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit...</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  // Our History Section
+  const HistorySection = () => {
+    return (
+      <div className="history-section">
+        <div className="section-header">
+          <h2>Our history</h2>
+          <hr />
+        </div>
+        <div className="history-content">
+          <p>(this is where there will be a coupke of images and blurb about the company and its history...)</p>
+        </div>
+      </div>
+    )
+  }
+
+
+  // MASTER RETURN
   return (
     <div className='about-page'>
       <PageHeader 
-        title="About Us" 
+        title="About us" 
         backgroundImage="/about-bg.webp" 
-      />    
+      />  
+      <ServiceSection/>  
+      <HistorySection/>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,8 @@
 import React from 'react'
 import LandingHeader from '../components/LandingHeader'
-import { FaWrench } from 'react-icons/fa'
-import { FaTemperatureThreeQuarters } from 'react-icons/fa6'
-import { PiGridNineLight } from "react-icons/pi";
 
 const Home = () => {
-  // Services Section
-  const ServiceSection = () => {
-    return (
-      <div className="services-section">
-        <div className="section-header">
-          <h2>Our Services</h2>
-          <hr />
-        </div>
-        <div className="services-cont">
-          <div className="service-item">
-            <FaWrench />
-            <h4>GENERAL PLUMBING & MAINTENANCE</h4>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit...</p>
-          </div>
-
-          <div className="service-item">
-            <FaTemperatureThreeQuarters />
-            <h4>HEATING</h4>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit...</p>
-          </div>
-
-          <div className="service-item">
-            <PiGridNineLight />
-            <h4>DRAINLAYING</h4>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit...</p>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
+  
   // Image Scroll Section #1
   const ImageScroll1 = () => <div className="image-scroll"></div>
   // Image Scroll Section #2
