711b87e79074301131e51ccf3ddcdc687671b492
caitlinanderson099 <caitlin.anderson099@gmail.com>
2025-09-26 15:51:50 +1200

    organised files and css file

--- src/pages/TestimonialGallery.jsx
+++ src/pages/TestimonialGallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react";
 import PageHeader from '../components/PageHeader'
 import { FaStar } from 'react-icons/fa'
 // import SEO from '../components/SEO';
@@ -50,49 +50,64 @@ const TestimonialGallery = () => {
     };
 
 
-// Gallery Section (Swiper)
-    const GallerySection = () => {
-        const images = [
-        "/project-images/bathroom-reno.jpeg",
-        "/project-images/drainlaying-img.jpeg",
-        "/project-images/bathroom-reno2.jpeg",
-        "/project-images/bathroom-reno.jpeg",
-        "/project-images/drainlaying-img.jpeg",
-        "/project-images/bathroom-reno2.jpeg"
-        ]
+const GallerySection = () => {
+  const images = [
+    "/project-images/bathroom-reno.jpeg",
+    "/project-images/drainlaying-img.jpeg",
+    "/project-images/bathroom-reno2.jpeg",
+    "/project-images/bathroom-reno.jpeg",
+    "/project-images/drainlaying-img.jpeg",
+    "/project-images/bathroom-reno2.jpeg",
+  ];
 
-        return (
-        <div className="gallery-section">
-            <div className="section-header">
-            <h2>Gallery</h2>
-            <hr />
-            </div>
+  const [selectedImage, setSelectedImage] = useState(null);
 
-            <Swiper
-            modules={[Navigation, Pagination]}
-            spaceBetween={20}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            breakpoints={{
-                640: { slidesPerView: 2 },   // 2 on tablets
-                1024: { slidesPerView: 3 },  // 3 on desktops
-            }}
-            className="gallery-swiper"
-            >
-            {images.map((src, index) => (
-                <SwiperSlide key={index}>
-                <img
-                    src={src}
-                    alt={`Gallery ${index + 1}`}
-                    className="gallery-img"
-                />
-                </SwiperSlide>
-            ))}
-            </Swiper>
+  return (
+    <div className="gallery-section">
+      <div className="section-header">
+        <h2>Gallery</h2>
+        <hr />
+      </div>
+
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={20}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: { slidesPerView: 2 }, // 2 on tablets
+          1024: { slidesPerView: 3 }, // 3 on desktops
+        }}
+        className="gallery-swiper"
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={src}
+              alt={`Gallery ${index + 1}`}
+              className="gallery-img"
+              onClick={() => setSelectedImage(src)} // open modal
+              style={{ cursor: "pointer" }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Modal */}
+      {selectedImage && (
+        <div className="modal-overlay" onClick={() => setSelectedImage(null)}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <button className="modal-close" onClick={() => setSelectedImage(null)}>
+              ✕
+            </button>
+            <img src={selectedImage} alt="Full View" className="modal-img" />
+          </div>
         </div>
-        )
-    };
+      )}
+    </div>
+  );
+};
 
      // Image Scroll Section #2
   const ImageScroll2 = () => <div className="image-scroll2"></div>
